7548296cbd0bd337a6cb476487e7186115923c5a
Oumoussi <abderahimamoussi1@gmail.com>
2024-03-05 13:46:35 +0100

    V1.0

---
+++ SignUp.vue
@@ -0,0 +1,87 @@
+
+<template>
+    <img class="logo" alt="Vue logo" src="../assets/logo.png">
+    <h1>SignUp</h1>
+    <div class="register">
+        <input type="email" placeholder="Entrez votre e-mail" v-model="email">
+        <input type="password" placeholder="Entrez votre mot de passe" v-model="password">
+        <button @click="register">S'enregistrer</button>
+    </div>
+</template>
+
+<script>
+import Register from '@/services/Register'
+    export default{
+        data () {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        watch: {
+            email (value) {
+                console.log('Email has changed', value)
+            }
+        },
+        methods:{
+            async register () {
+                const response = await Register.register({
+                    email: this.email,
+                    password: this.password
+                })
+                console.log(response.data)
+            }
+        },
+        mounted () {
+            setTimeout(function(){
+                this.email = 'Hello'
+                }, 1000)
+        }
+    }
+
+</script>
+
+<style>
+    .logo{
+        width: 100px;
+    }
+
+    .register input {
+        padding: 10px; 
+        margin: auto auto; 
+        margin-bottom: 20px; 
+        display: inline-block; 
+        border: 1px solid #ccc;
+        border-radius: 4px; 
+        box-sizing: border-box; 
+        width: 25%; 
+        display: block;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); 
+        transition: border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out; 
+    }
+
+    .register input:focus {
+        border-color: #4CAF50; 
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4); 
+        outline: none; 
+    }
+
+    .register button{
+        background-color: #4CAF50; 
+        border: none; 
+        color: white; 
+        padding: 15px 32px; 
+        text-align: center; 
+        text-decoration: none;
+        display: inline-block; 
+        font-size: 16px; 
+        margin: 4px 2px;
+        cursor: pointer; 
+        border-radius: 8px;
+        transition-duration: 0.4s;
+    }
+    .register button:hover {
+        background-color: cyan; 
+        color: black; 
+    }
+</style>@/services/Register
---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Bienvenue ${req.body.email}, ${req.body.password}!!!`
+  })
+})
+app.listen(process.env.PORT || 8081)
